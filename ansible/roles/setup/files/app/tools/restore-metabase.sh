f6d6ec72e274348492142849ba0c2d26d07820cc
Alan Le Ruyet <alan.leruyet@free.fr>
2022-11-03 12:46:23 +0100

    feat(metabase): scripts backup restore

---
+++ ansible/roles/setup/files/app/tools/restore-metabase.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -euo pipefail
+#Needs to be run as sudo
+
+readonly BACKUP_FILE=${1:?"Please provide a backup file path $(echo '' && find /mnt2/backups/metabase)"}; shift;
+
+stop_container() {
+  bash /opt/doctrina/stop-app.sh metabase
+}
+
+restart_container() {
+  local CURRENT_BRANCH
+  CURRENT_BRANCH="$(git --git-dir=/opt/doctrina/repository/.git rev-parse --abbrev-ref HEAD)"
+
+  NO_UPDATE=true bash /opt/doctrina/start-app.sh "${CURRENT_BRANCH}" --no-deps metabase
+}
+
+function restore_metabase(){
+  echo "Restauration de la base metabase..."
+
+  stop_container
+  tar -xvf "${BACKUP_FILE}" -C /opt/labonnealternance/data/metabase
+  restart_container
+}
+
+                                                                                                                                                     1,1           Top
