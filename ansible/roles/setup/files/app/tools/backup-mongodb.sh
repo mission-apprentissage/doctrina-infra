0c54374881502b5e937eab041aa5748422665ada
Kevin Barnoin <k.barnoin@gmail.com>
2022-12-22 16:33:20 +0100

    feat: update mongodb command to preserve memory

--- ansible/roles/setup/files/app/tools/backup-mongodb.sh
+++ ansible/roles/setup/files/app/tools/backup-mongodb.sh
@@ -10,7 +10,7 @@ readonly BACKUP_FILE="${BACKUP_LOCAL_DIR}/mongodb-$(date +'%Y-%m-%d_%H%M%S').gpg
 function backup() {
   echo "Creating backup..."
   mkdir -p "${BACKUP_LOCAL_DIR}"
-  docker exec lba_mongodb bash -c "mongodump --gzip --archive -u backup -p {{ vault.LBA_MONGODB_BACKUP_PASSWORD }}" \
+  docker exec lba_mongodb bash -c "mongodump -j=2 --gzip --archive -u backup -p {{ vault.LBA_MONGODB_BACKUP_PASSWORD }}" \
   | bash "${SCRIPT_DIR}/gpg/encrypt.sh" >"${BACKUP_FILE}"
 }
 
